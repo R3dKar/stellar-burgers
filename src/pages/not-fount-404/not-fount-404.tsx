a6b4ad69b9fd3df5110a17441c68a257e3692fe2
R3dKar <aazz.140720045@gmail.com>
2025-05-18 01:30:34 +0300

    fix: fix 404 page centering

--- src/pages/not-fount-404/not-fount-404.tsx
+++ src/pages/not-fount-404/not-fount-404.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
+import styles from './not-fount-404.module.css';
+import clsx from 'clsx';
 
 export const NotFound404: FC = () => (
-  <h3 className={`pb-6 text text_type_main-large`}>
+  <h3 className={clsx('pb-6', 'text', 'text_type_main-large', styles.text)}>
     Страница не найдена. Ошибка 404.
   </h3>
 );
